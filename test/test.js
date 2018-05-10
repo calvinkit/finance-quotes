16bef953d229e44c49610dd42f955d80731ed4ea
Calvin Lai <calvin.lai@scotiabank.com>
2018-05-09 22:10:03 -0400

    relocation of files

--- test/test.js
+++ test/test.js
@@ -6,12 +6,12 @@ var httpProxy = process.env.HTTP_PROXY?new httpProxyAgent(process.env.HTTP_PROXY
 var avkey = '';
 var qlkey = '';
 
-var pricer = require('./cnbc');
-var q = new pricer(httpsProxy, qlkey);
+var quotes = require('./../lib/index');
+
+var q = new quotes.cnbc(httpsProxy, qlkey);
 var security = { ticker:'GOOG',yticker:'GOOG', country:'United States' };
 q.getprice(security, console.log, console.log)
 
-var pricer = require('./quandl');
-var q = new pricer(httpsProxy, qlkey);
+var q = new quotes.quandl(httpsProxy, qlkey);
 var security = { ticker:'043602_F_ALL',yticker:'043602_F_ALL', country:'CFTC' };
 q.getprice(security, console.log, console.log)
